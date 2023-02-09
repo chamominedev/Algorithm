3934e9e0c06b5782489a2d28301e2488b082a7dd
JaeHo Kim <97279920+chamominedev@users.noreply.github.com>
2023-02-09 11:56:51 +0900

    [level 0] Title: OX퀴즈, Time: 0.15 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        for (int i = 0; i < quiz.length; i++) {
+            String[] arr = quiz[i].split(" ");
+
+            int a = Integer.parseInt(arr[0]);
+            int b = Integer.parseInt(arr[2]);
+            int c = Integer.parseInt(arr[4]);
+
+            if (arr[1].equals("-") ? a - b == c : a + b == c) {
+                answer[i] = "O";
+            } else {
+                answer[i] = "X";
+            }
+
+        }
+        return answer;
+    }
+}
