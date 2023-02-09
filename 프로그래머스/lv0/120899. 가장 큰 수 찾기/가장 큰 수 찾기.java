d7eb5891754c52f6c201bc35d87410645bff430e
JaeHo Kim <97279920+chamominedev@users.noreply.github.com>
2023-02-09 13:57:20 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        
+        int maxNum = 0;
+        int indexNum = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] > maxNum) {
+                maxNum = array[i];
+                indexNum = i;
+            }
+        }
+        
+        answer[0] = maxNum;
+        answer[1] = indexNum;
+        
+        return answer;
+    }
+}
